272db6fbc764db6371abc085d1d0b11e42d66d5c
Tristan F <LeoDog896@hotmail.com>
2023-01-21 16:08:17 -0500

    feat: linting

--- codemirror.ts
+++ codemirror.ts
@@ -1,9 +1,12 @@
-import {EditorView, minimalSetup} from "codemirror"
+import { EditorView, minimalSetup } from "codemirror";
+import { linter, Diagnostic } from "@codemirror/lint";
+import init, { lint } from "./pkg/pest_site.js";
 
 const editorDom = document.querySelector<HTMLDivElement>(".editor")!;
 const gridDom = document.querySelector<HTMLDivElement>(".editor-grid")!;
 const inputDom = document.querySelector<HTMLDivElement>(".editor-input")!;
-const outputDom = document.querySelector<HTMLTextAreaElement>(".editor-output")!;
+const outputDom =
+  document.querySelector<HTMLTextAreaElement>(".editor-output")!;
 const modeBtn = document.querySelector<HTMLButtonElement>("#modeBtn")!;
 
 // let windowHeight = window.innerHeight;
@@ -35,43 +38,64 @@ const modeBtn = document.querySelector<HTMLButtonElement>("#modeBtn")!;
 //   },
 // });
 
-// CodeMirror.registerHelper("lint", "pest", function (text) {
-//   if (window.lint) {
-//     let doc = CodeMirror.Doc(text);
-//     let errors = window.lint(text);
-//     let mapped = [];
 
-//     for (let i = 0; i < errors.length; i++) {
-//       let from = doc.clipPos(eval("CodeMirror.Pos" + errors[i].get("from")));
-//       let to = doc.clipPos(eval("CodeMirror.Pos" + errors[i].get("to")));
-
-//       if (from.line === to.line && from.ch === to.ch) {
-//         to.ch += 1;
-//         to = doc.clipPos(to);
-//       }
-
-//       if (from.line === to.line && from.ch === to.ch) {
-//         from.ch -= 1;
-//         from = doc.clipPos(from);
-//       }
-
-//       mapped.push({
-//         message: errors[i].get("message"),
-//         from: from,
-//         to: to,
-//       });
-//     }
-
-//     return mapped;
-//   } else {
-//     return [];
-//   }
-// });
 let current_data = null;
 
+const pestLinter = linter((view) => {
+  if (lint) {
+    let errors = lint(view.state.doc.toString());
+    let diagnostics: Diagnostic[] = [];
+
+    for (let i = 0; i < errors.length; i++) {
+      let [fromLine, fromCh] = errors[i].get("from").slice(1, -1).split(", ").map(x => parseInt(x));
+      let [toLine, toCh] = errors[i].get("to").slice(1, -1).split(", ").map(x => parseInt(x));
+
+      if (fromLine === toLine && fromCh === toCh) {
+        toCh += 1;
+      }
+
+      if (fromLine === toLine && fromCh === toCh) {
+        fromCh -= 1;
+      }
+
+      diagnostics.push({
+        severity: "error",
+        message: errors[i].get("message"),
+        from: view.state.doc.line(fromLine + 1).from + fromCh,
+        to: view.state.doc.line(toLine + 1).from + toCh,
+      });
+    }
+
+    return diagnostics;
+  } else {
+    return [];
+  }
+});
 
 let grammar = document.querySelector(".editor-grammar")!;
 const editor = new EditorView({
-    extensions: [minimalSetup],
-    parent: grammar
-})
+  extensions: [minimalSetup, pestLinter],
+  parent: grammar,
+});
+
+(window as any).set_current_data = function() {
+  if (current_data) {
+    var select = document.querySelector<HTMLSelectElement>("editor-input-select")!;
+
+    for (var i = 0; i < select.options.length; i++) {
+      if (select.options[i].value === current_data["rule"]) {
+        select.selectedIndex = i;
+        var event = new Event('change');
+        select.dispatchEvent(event);
+        break;
+      }
+    }
+
+    const input = document.querySelector<HTMLInputElement>(".editor-input-text")!;
+    input.value = current_data["input"];
+
+    current_data = null;
+  }
+}
+
+init()
