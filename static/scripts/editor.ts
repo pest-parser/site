0640fb7e129c87ca63f1b3a38ba78bf743905071
Tristan F <LeoDog896@hotmail.com>
2023-07-30 15:28:05 -0400

    feat: prettier, eslint, and package updates

--- static/scripts/editor.ts
+++ static/scripts/editor.ts
@@ -1,30 +1,33 @@
-declare var CodeMirror: any;
+declare const CodeMirror: any;
 
-import Split from 'split.js'
-import init, { lint, format } from '../../pkg/pest_site.js';
-import {initShareButton} from "./shareButton";
+import Split from "split.js";
+import init, { lint, format } from "../../pkg/pest_site.js";
+import { initShareButton } from "./shareButton";
 
 let loaded = false;
 
 const editorDom = document.querySelector<HTMLLinkElement>(".editor")!;
 const gridDom = document.querySelector<HTMLDivElement>(".editor-grid")!;
 const inputDom = document.querySelector<HTMLDivElement>(".editor-input")!;
-const outputDom = document.querySelector<HTMLTextAreaElement>(".editor-output")!;
+const outputDom =
+  document.querySelector<HTMLTextAreaElement>(".editor-output")!;
 const modeBtn = document.querySelector<HTMLButtonElement>("#modeBtn")!;
 const formatBtn = document.querySelector<HTMLButtonElement>("#formatBtn")!;
 
 let windowHeight = window.innerHeight;
-var current_data = null;
 
 CodeMirror.defineSimpleMode("pest", {
   start: [
     { regex: /"/, token: "string", next: "string" },
-    { regex: /'(?:[^'\\]|\\(?:[nrt0'"]|x[\da-fA-F]{2}|u\{[\da-fA-F]{6}\}))'/, token: "string" },
+    {
+      regex: /'(?:[^'\\]|\\(?:[nrt0'"]|x[\da-fA-F]{2}|u\{[\da-fA-F]{6}\}))'/,
+      token: "string",
+    },
     { regex: /\/\/.*/, token: "comment" },
     { regex: /\d+/, token: "number" },
     { regex: /[~|*+?&!]|(\.\.)/, token: "operator" },
-    { regex: /[a-zA-Z_]\w*/, token: "variable" },
-    { regex: /=/, next: "mod" }
+    { regex: /[a-zA-Z_]\w*/, token: "constiable" },
+    { regex: /=/, next: "mod" },
   ],
   mod: [
     { regex: /\{/, next: "start" },
@@ -32,23 +35,23 @@ CodeMirror.defineSimpleMode("pest", {
   ],
   string: [
     { regex: /"/, token: "string", next: "start" },
-    { regex: /(?:[^\\"]|\\(?:.|$))*/, token: "string" }
+    { regex: /(?:[^\\"]|\\(?:.|$))*/, token: "string" },
   ],
   meta: {
     dontIndentStates: ["comment"],
-    lineComment: "//"
-  }
+    lineComment: "//",
+  },
 });
 
 CodeMirror.registerHelper("lint", "pest", function (text) {
   if (loaded) {
-    var doc = CodeMirror.Doc(text);
-    var errors = lint(text);
-    var mapped: { message: string, from: number, to: number }[] = [];
+    const doc = CodeMirror.Doc(text);
+    const errors = lint(text);
+    const mapped: { message: string; from: number; to: number }[] = [];
 
-    for (var i = 0; i < errors.length; i++) {
-      var from = doc.clipPos(eval("CodeMirror.Pos" + errors[i].get("from")));
-      var to = doc.clipPos(eval("CodeMirror.Pos" + errors[i].get("to")));
+    for (let i = 0; i < errors.length; i++) {
+      let from = doc.clipPos(eval("CodeMirror.Pos" + errors[i].get("from")));
+      let to = doc.clipPos(eval("CodeMirror.Pos" + errors[i].get("to")));
 
       if (from.line === to.line && from.ch === to.ch) {
         to.ch += 1;
@@ -63,7 +66,7 @@ CodeMirror.registerHelper("lint", "pest", function (text) {
       mapped.push({
         message: errors[i].get("message"),
         from: from,
-        to: to
+        to: to,
       });
     }
 
@@ -73,49 +76,28 @@ CodeMirror.registerHelper("lint", "pest", function (text) {
   }
 });
 
-var grammar = document.getElementsByClassName("editor-grammar")[0];
-var myCodeMirror = CodeMirror.fromTextArea(grammar, {
+const grammar = document.getElementsByClassName("editor-grammar")[0];
+const myCodeMirror = CodeMirror.fromTextArea(grammar, {
   mode: "pest",
   lint: "pest",
   theme: "pest",
   placeholder: "Grammar",
 });
 
-initShareButton({myCodeMirror});
-
-
-global.set_current_data = function() {
-  if (current_data) {
-    const select = document.querySelector<HTMLSelectElement>(".editor-input-select")!;
-
-    for (var i = 0; i < select.options.length; i++) {
-      if (select.options[i].value === current_data["rule"]) {
-        select.selectedIndex = i;
-        var event = new Event('change');
-        select.dispatchEvent(event);
-        break;
-      }
-    }
-
-    var input = document.querySelector<HTMLTextAreaElement>(".editor-input-text")!;
-    input.value = current_data["input"];
-
-    current_data = null;
-  }
-}
+initShareButton({ myCodeMirror });
 
 function doFormat() {
   if (!loaded || !myCodeMirror) {
-    return
+    return;
   }
 
-  let grammar = myCodeMirror.getValue();
-  let formatted = format(grammar);
+  const grammar = myCodeMirror.getValue();
+  const formatted = format(grammar);
   myCodeMirror.setValue(formatted);
 }
 
 let split: Split.Instance | null = null;
-let sizes = JSON.parse(localStorage.getItem('split-sizes') ?? "[33, 33, 33]");
+let sizes = JSON.parse(localStorage.getItem("split-sizes") ?? "[33, 33, 33]");
 if (!Array.isArray(sizes) || sizes.length !== 3) {
   console.warn("Invalid split sizes", sizes);
   sizes = [33, 33, 33];
@@ -123,13 +105,13 @@ if (!Array.isArray(sizes) || sizes.length !== 3) {
 
 function makeResizable(wideMode: boolean) {
   if (wideMode) {
-    split = Split(['.CodeMirror', '.editor-input', '.output-wrapper'], {
+    split = Split([".CodeMirror", ".editor-input", ".output-wrapper"], {
       sizes,
       onDragEnd: function (_sizes) {
         sizes = _sizes;
-        localStorage.setItem('split-sizes', JSON.stringify(sizes))
+        localStorage.setItem("split-sizes", JSON.stringify(sizes));
       },
-    })
+    });
   } else {
     if (split) {
       split.destroy();
@@ -143,8 +125,8 @@ function wideMode() {
   inputDom.classList.add("wide-input");
   editorDom.classList.add("wide-editor");
   gridDom.classList.add("flex-editor");
-  const upperHeight = document.querySelector('#modeBtn')!.scrollHeight + 30;
-  gridDom.setAttribute('style', `height: ${windowHeight - upperHeight}px`);
+  const upperHeight = document.querySelector("#modeBtn")!.scrollHeight + 30;
+  gridDom.setAttribute("style", `height: ${windowHeight - upperHeight}px`);
   myCodeMirror.setSize(null, outputDom.clientHeight - 20);
   makeResizable(true);
   window.scrollTo(0, document.body.scrollHeight);
@@ -164,4 +146,4 @@ function restore() {
 modeBtn.onclick = wideMode;
 formatBtn.onclick = doFormat;
 
-init().then(() => loaded = true);
+init().then(() => (loaded = true));
--- static/scripts/shareButton.ts
+++ static/scripts/shareButton.ts
@@ -1,76 +1,84 @@
-import { compressToEncodedURIComponent, decompressFromEncodedURIComponent } from "lz-string";
+import {
+  compressToEncodedURIComponent,
+  decompressFromEncodedURIComponent,
+} from "lz-string";
 
 let copyButton;
 let copyButtonOriginalText;
 let shareLinkWarning;
 
 export function initShareButton({ myCodeMirror }) {
-    shareLinkWarning = document.querySelector<HTMLParagraphElement>("p#shareLinkWarning");
-    tryLoadFromShareLink(myCodeMirror);
-    copyButton = document.querySelector<HTMLButtonElement>("button#shareLinkBtn");
-    copyButtonOriginalText = copyButton.innerText;
-    copyButton.onclick = () => copyShareLink(myCodeMirror);
+  shareLinkWarning =
+    document.querySelector<HTMLParagraphElement>("p#shareLinkWarning");
+  tryLoadFromShareLink(myCodeMirror);
+  copyButton = document.querySelector<HTMLButtonElement>("button#shareLinkBtn");
+  copyButtonOriginalText = copyButton.innerText;
+  copyButton.onclick = () => copyShareLink(myCodeMirror);
 }
 
 let copyButtonTimerHandle: number | undefined = undefined;
 
 function copyShareLink(codeMirror) {
-    clearTimeout(copyButtonTimerHandle);
-    const url = shareURL(codeMirror);
-    if (url.length > 2000) {
-        setShareLinkLengthWarning();
-    } else {
-        clearShareLinkWarning();
-    }
-    navigator.clipboard.writeText(url);
-    copyButton.classList.add("copied");
-    copyButton.innerText = "Copied";
-    copyButtonTimerHandle = window.setTimeout(
-        () => { copyButton.classList.remove("copied"); copyButton.innerText = copyButtonOriginalText; },
-        1400
-    );
+  clearTimeout(copyButtonTimerHandle);
+  const url = shareURL(codeMirror);
+  if (url.length > 2000) {
+    setShareLinkLengthWarning();
+  } else {
+    clearShareLinkWarning();
+  }
+  navigator.clipboard.writeText(url);
+  copyButton.classList.add("copied");
+  copyButton.innerText = "Copied";
+  copyButtonTimerHandle = window.setTimeout(() => {
+    copyButton.classList.remove("copied");
+    copyButton.innerText = copyButtonOriginalText;
+  }, 1400);
 }
 
 function tryLoadFromShareLink(codeMirror) {
-    let url = new URL(window.location.href);
-    let gdata = url.searchParams.get("g");
-    if (gdata) {
-        const decoded = decodeShareData(gdata);
-        codeMirror.setValue(decoded["grammar"]);
-        const inputEditor = document.querySelector<HTMLTextAreaElement>("textarea.editor-input-text")!;
-        inputEditor.value = decoded["input"];
-    }
+  const url = new URL(window.location.href);
+  const gdata = url.searchParams.get("g");
+  if (gdata) {
+    const decoded = decodeShareData(gdata);
+    codeMirror.setValue(decoded["grammar"]);
+    const inputEditor = document.querySelector<HTMLTextAreaElement>(
+      "textarea.editor-input-text",
+    )!;
+    inputEditor.value = decoded["input"];
+  }
 }
 
 function shareData(codeMirror) {
-    let data = {};
-    data["grammar"] = codeMirror.getValue();
-    data["input"] = document.querySelector<HTMLTextAreaElement>("textarea.editor-input-text")!.value;
-    return data;
+  return {
+    grammar: codeMirror.getValue(),
+    input: document.querySelector<HTMLTextAreaElement>(
+      "textarea.editor-input-text",
+    )!.value,
+  };
 }
 
 function shareURL(codeMirror) {
-    const gdata = encodeShareData(shareData(codeMirror));
-    const url = new URL(window.location.href);
-    url.searchParams.set("g", gdata);
-    url.hash = "editor";
-    return url.toString();
+  const gdata = encodeShareData(shareData(codeMirror));
+  const url = new URL(window.location.href);
+  url.searchParams.set("g", gdata);
+  url.hash = "editor";
+  return url.toString();
 }
 
 function setShareLinkLengthWarning() {
-    shareLinkWarning.innerText = "Share link > 2000 chars; it may not work."
-    shareLinkWarning.style.display = null;
+  shareLinkWarning.innerText = "Share link > 2000 chars; it may not work.";
+  shareLinkWarning.style.display = null;
 }
 
 function clearShareLinkWarning() {
-    shareLinkWarning.innerText = "";
-    shareLinkWarning.style.display = "none";
+  shareLinkWarning.innerText = "";
+  shareLinkWarning.style.display = "none";
 }
 
 function encodeShareData(data: any) {
-    return compressToEncodedURIComponent(JSON.stringify(data));
+  return compressToEncodedURIComponent(JSON.stringify(data));
 }
 
 function decodeShareData(encoded: string) {
-    return JSON.parse(decompressFromEncodedURIComponent(encoded));
+  return JSON.parse(decompressFromEncodedURIComponent(encoded));
 }
