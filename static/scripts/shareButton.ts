767eaac622114cbfbe6adbb9278e4a127fc3b805
Omer Abdulaziz <omerabdi@protonmail.com>
2024-09-12 05:27:44 +0300

    Embed selected rule in the url when sharing. (#65)
    * Embed selected rule in the url when sharing.
    
    * fix: check for undefined

--- static/scripts/shareButton.ts
+++ static/scripts/shareButton.ts
@@ -45,6 +45,9 @@ function tryLoadFromShareLink(codeMirror) {
       "textarea.editor-input-text",
     )!;
     inputEditor.value = decoded["input"];
+    if (decoded["selectedRule"]) {
+      localStorage.setItem("last-selected-rule", decoded["selectedRule"]);
+    }
   }
 }
 
@@ -54,6 +57,7 @@ function shareData(codeMirror) {
     input: document.querySelector<HTMLTextAreaElement>(
       "textarea.editor-input-text",
     )!.value,
+    selectedRule: localStorage.getItem("last-selected-rule") || "",
   };
 }
 
